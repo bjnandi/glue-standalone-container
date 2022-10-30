9442f19a516b0f717e615e9ff91cb99c3ef85631
SABBiR <team.cloudboost@gmail.com>
2022-10-30 23:36:24 +0600

    devoloping

--- Dockerfiles/Entrypoint/backend-docker-entrypoint.sh
+++ Dockerfiles/Entrypoint/backend-docker-entrypoint.sh
@@ -4,7 +4,7 @@ cd /public_html;
 sed -i "s,DB_DATABASE.*,DB_DATABASE=$APP_DB,g" /public_html/$REPO_PROJECT_FOLDER/.env;
 sed -i "s,DB_USERNAME.*,DB_USERNAME=$APP_DB_USER,g" /public_html/$REPO_PROJECT_FOLDER/.env;
 sed -i "s,DB_PASSWORD.*,DB_PASSWORD=$APP_DB_PASS,g" /public_html/$REPO_PROJECT_FOLDER/.env;
-sed -i "s,DB_HOST.*,DB_HOST=$CNTR_DB,g" /public_html/$REPO_PROJECT_FOLDER/.env;
+sed -i "s,DB_HOST.*,DB_HOST=$DB_HOST,g" /public_html/$REPO_PROJECT_FOLDER/.env;
 chown -R www-data:www-data /public_html/$REPO_PROJECT_FOLDER/storage; 
 chown -R www-data:www-data /public_html/$REPO_PROJECT_FOLDER/bootstrap/cache;
 /usr/sbin/service php8.1-fpm start
