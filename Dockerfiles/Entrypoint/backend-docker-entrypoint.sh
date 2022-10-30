11d0332b9a6c2c83a64071a4631ba43cd4c63b20
SABBiR <team.cloudboost@gmail.com>
2022-10-30 22:48:38 +0600

    First Commit

---
+++ Dockerfiles/Entrypoint/backend-docker-entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd /public_html;
+# Modifying .env for project
+sed -i "s,DB_DATABASE.*,DB_DATABASE=$APP_DB,g" /public_html/$REPO_PROJECT_FOLDER/.env;
+sed -i "s,DB_USERNAME.*,DB_USERNAME=$APP_DB_USER,g" /public_html/$REPO_PROJECT_FOLDER/.env;
+sed -i "s,DB_PASSWORD.*,DB_PASSWORD=$APP_DB_PASS,g" /public_html/$REPO_PROJECT_FOLDER/.env;
+sed -i "s,DB_HOST.*,DB_HOST=$CNTR_DB,g" /public_html/$REPO_PROJECT_FOLDER/.env;
+chown -R www-data:www-data /public_html/$REPO_PROJECT_FOLDER/storage; 
+chown -R www-data:www-data /public_html/$REPO_PROJECT_FOLDER/bootstrap/cache;
+/usr/sbin/service php8.1-fpm start
+tail -f /dev/null
---
+++ Dockerfiles/Entrypoint/frontend-docker-entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Modifying Website parameters
+sed -i "s/IP_ADDRESS_OR_DOMAIN/$WEB_ADDRESS/g" /etc/nginx/conf.d/default.conf;
+sed -i "s,/public_html,/public_html/$REPO_PROJECT_FOLDER/public,g" /etc/nginx/conf.d/default.conf;
+sed -i "s,fastcgi_pass.*,fastcgi_pass $CNTR_BACK:9000;,g" /etc/nginx/conf.d/default.conf;
+# Modifying .env for project
+sed -i "s,DB_DATABASE.*,DB_DATABASE=$APP_DB,g" /public_html/$REPO_PROJECT_FOLDER/.env;
+sed -i "s,DB_USERNAME.*,DB_USERNAME=$APP_DB_USER,g" /public_html/$REPO_PROJECT_FOLDER/.env;
+sed -i "s,DB_PASSWORD.*,DB_PASSWORD=$APP_DB_PASS,g" /public_html/$REPO_PROJECT_FOLDER/.env;
+sed -i "s,DB_HOST.*,DB_HOST=$CNTR_DB,g" /public_html/$REPO_PROJECT_FOLDER/.env;
+tail -f /dev/null
