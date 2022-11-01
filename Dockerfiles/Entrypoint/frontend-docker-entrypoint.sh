4ff8c8ca07742884c0f6fb41a4deee688fb041e5
Team-Cloudboost <113457895+Team-Cloudboost@users.noreply.github.com>
2022-11-01 12:42:43 -0700

    Update frontend-docker-entrypoint.sh

--- Dockerfiles/Entrypoint/frontend-docker-entrypoint.sh
+++ Dockerfiles/Entrypoint/frontend-docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Modifying Website parameters
-sed -i "s/IP_ADDRESS_OR_DOMAIN/$WEB_ADDRESS/g" /etc/nginx/conf.d/default.conf;
-sed -i "s,/public_html,/public_html/$REPO_PROJECT_FOLDER/public,g" /etc/nginx/conf.d/default.conf;
+sed -i "s/IP_ADDRESS_OR_DOMAIN/$SERVER_NAME/g" /etc/nginx/conf.d/default.conf;
+sed -i "s,/public_html.*,/public_html/$REPO_PROJECT_FOLDER/public,g" /etc/nginx/conf.d/default.conf;
 sed -i "s,fastcgi_pass.*,fastcgi_pass $BACK_HOST:9000;,g" /etc/nginx/conf.d/default.conf;
 # Modifying .env for project
 sed -i "s,DB_DATABASE.*,DB_DATABASE=$APP_DB,g" /public_html/$REPO_PROJECT_FOLDER/.env;
